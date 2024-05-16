6a5f16fb8d2041ee0ba2e560e14bac03864d559d
IlyaKorchinskiy <korchinskiy.ilya@gmail.com>
2024-05-16 13:22:21 +0200

    scripts

--- parking-booking-infrastructure/src/main/resources/db/migration/V1_0_0__initial_tables.sql
+++ parking-booking-infrastructure/src/main/resources/db/migration/V1_0_0__initial_tables.sql
@@ -14,7 +14,27 @@ create table booking.bookings
 create table booking.booking_items
 (
     start_date_time timestamp with time zone not null,
-    vehicle_number character not null ,
-    booking_id uuid not null ,
-    constraint
-)
+    vehicle_number  varchar(8)               not null,
+    booking_id      uuid                     not null,
+    constraint booking_items_pkey primary key (start_date_time, vehicle_number),
+    constraint bookings_fk foreign key (booking_id) references bookings (id) on delete cascade
+);
+
+create table booking.parkings
+(
+    id           uuid          not null,
+    address      varchar(50)   not null,
+    coordinate_x numeric(8, 6) not null,
+    coordinate_y numeric(8, 6) not null,
+    total_places smallint      not null,
+    constraint parkings_pkey primary key (id)
+);
+
+create table booking.slots
+(
+    start_date_time timestamp with time zone not null,
+    parking_id      uuid                     not null,
+    available       smallint                 not null,
+    constraint slots_pkey primary key (start_date_time, parking_id),
+    constraint parkings_fk foreign key (parking_id) references parkings (id)
+);
---
+++ parking-booking-infrastructure/src/main/resources/db/migration/V1_0_1__fill_parkings.sql
@@ -0,0 +1,13 @@
+insert into booking.parkings (id, address, coordinate_x, coordinate_y, total_places)
+values ('9ad24d2f-ca4f-4a5e-b39a-15a64ff37016', 'C. Jovellanos, 7, 33003 Oviedo, Asturias', 43.363439, -5.845234, 30),
+       ('3cbb709d-2324-427f-b655-43b8016c3ed4', 'C. Manuel García Conde, 13, 33001 Oviedo, Asturias', 43.364561,
+        -5.845149, 20),
+       ('86e31a35-b015-432a-a9c8-9461c94dab15', 'C/ del Gral. Elorza, 17, 33001 Oviedo, Asturias', 43.365473, -5.840754,
+        24),
+       ('beec7b90-a6d3-4c52-b4e3-939df9ca5904', 'Av. Torrelavega, 11, 33010 Oviedo, Asturias', 43.363695, -5.837892,
+        36),
+       ('ce952f14-a3ba-4718-a31a-b9b24c77e00f', 'C/ Darío de Regoyos, 39, 33008 Oviedo, Asturias', 43.362108, -5.834013,
+        40),
+       ('29313111-f099-4cef-9421-9e8d09a6aa38', 'C. Chavia, 1, 33008 Oviedo, Asturias', 43.360747, -5.836202, 16),
+       ('1797523b-20f9-4121-b134-6a62fc41edf3', 'Pl. Corrada del Obispo, 33003 Oviedo, Asturias', 43.361683, -5.843144,
+        12);
