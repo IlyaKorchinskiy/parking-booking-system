7404539ff9949af96c00102f0525d093a8cc0769
IlyaKorchinskiy <korchinskiy.ilya@gmail.com>
2024-05-22 11:24:38 +0200

    fix validation

--- parking-booking-infrastructure/src/main/java/ru/korchinskiy/parking/booking/infrastructure/rest/exception/RestExceptionHandler.java
+++ parking-booking-infrastructure/src/main/java/ru/korchinskiy/parking/booking/infrastructure/rest/exception/RestExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import openapi.model.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.korchinskiy.parking.booking.application.exception.BusinessValidationException;
@@ -13,8 +14,8 @@ import ru.korchinskiy.parking.booking.application.exception.EntityNotFoundExcept
 @ControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(BusinessValidationException.class)
-    public ResponseEntity<ErrorResponseDto> handleException(BusinessValidationException exception) {
+    @ExceptionHandler({BusinessValidationException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponseDto> handleException(Exception exception) {
         log.warn("Exception happened: {}", exception.getMessage());
         return new ResponseEntity<>(new ErrorResponseDto(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
@@ -26,7 +27,7 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDto> handleException(Exception exception) {
+    public ResponseEntity<ErrorResponseDto> handleUnknownException(Exception exception) {
         log.error("Exception happened: {}", exception.getMessage());
         return new ResponseEntity<>(
                 new ErrorResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()),
